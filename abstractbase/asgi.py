a98f81f851cf00d90c90b377ae99441a59080041
joe duop <johannnezwatulo@outlook.com>
2020-10-12 00:11:46 +0300

    project created accountsapp created and registered in the settings. templatesettings updated

---
+++ abstractbase/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for abstractbase project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abstractbase.settings')
+
+application = get_asgi_application()
